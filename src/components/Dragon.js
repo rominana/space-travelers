c418230f720d4049f88eefc1f5ec1da0fdb8bb71
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-08-23 03:31:20 -0300

    Update Dragon.js component

--- src/components/Dragon.js
+++ src/components/Dragon.js
@@ -1,18 +1,29 @@
-import planet from '../assets/images/planet.png';
+import PropTypes from 'prop-types';
 
-const Dragon = () => (
-  <div className="dragon-container container">
-    <img className="dragon-container-img" src={planet} alt="" />
-    <div className="dragon-info">
-      <h2 className="dragon-info-title">Dragon&apos;s Name</h2>
-      <p className="dragon-info-description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid placeat ipsa deserunt fugit
-        , consequatur quae, ad vero perspiciatis nesciunt fugiat magnam iusto aut suscipit impedit
-        molestiae modi corporis tempora pariatur?
-      </p>
-      <button type="button" className="dragon-info-button">Reserve Dragon</button>
+const Dragon = (props) => {
+  const {
+    id, name, type, flickrImages0,
+  } = props;
+
+  return (
+    <div className="dragon-container container" id={id}>
+      <img className="dragon-container-img" src={flickrImages0} alt="" />
+      <div className="dragon-info">
+        <h2 className="dragon-info-title">{name}</h2>
+        <p className="dragon-info-description">
+          {type}
+        </p>
+        <button type="button" className="dragon-info-button">Reserve Dragon</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
+Dragon.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  flickrImages0: PropTypes.string.isRequired,
+};
 
 export default Dragon;
