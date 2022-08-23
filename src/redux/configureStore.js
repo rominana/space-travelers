971fb760b139c1d6f3c79f7c73bef7a41c7095a3
Romina <romina.pati@gmail.com>
2022-08-23 11:04:43 -0400

    Modify configureStore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
-// import missions from './missions';
-// import dragons from './dragons';
-
 import rocketsReducer from './RocketSlice';
 
 const store = configureStore({
   reducer: {
-    //     missions: missions.reducer,
-    //     dragons: dragons.reducer,
     rockets: rocketsReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
