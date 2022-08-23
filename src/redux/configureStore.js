55365b22de646cfa12301928c344defdec36703b
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-08-23 03:33:02 -0300

    Update store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import missions from './missions';
 import rockets from './rockets';
-import dragons from './dragons';
+import { dragonsReducer } from './dragons';
 
 const store = configureStore({
   reducer: {
     missions: missions.reducer,
     rockets: rockets.reducer,
-    dragons: dragons.reducer,
+    dragons: dragonsReducer.reducer,
   },
-});
+}, applyMiddleware(thunk));
 
 export default store;
