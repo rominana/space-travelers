02513c7505f9fdbb7df826fcdd61be266bd0cde7
Romina <romina.pati@gmail.com>
2022-08-24 10:36:28 -0400

    Add test to reducers and functions on Rockets

--- src/__tests__/rockets.test.js
+++ src/__tests__/rockets.test.js
@@ -4,6 +4,7 @@ import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import store from '../redux/configureStore';
 import RocketsList from '../components/RocketList';
+import { fetchRocketsApi } from '../redux/RocketSlice';
 
 const renderRocketList = () => {
   const { container } = render(
@@ -17,5 +18,12 @@ const renderRocketList = () => {
 describe('Test Rocket page', () => {
   test('Match Snapshot & render Rocket List on screen', () => {
     expect(renderRocketList().firstChild).toMatchSnapshot();
+  });
+});
+
+describe('Test rocket reducers and functions', () => {
+  it('Fetches data from API', async () => {
+    await store.dispatch(fetchRocketsApi());
+    expect(store.getState().rockets).toBeTruthy();
   });
 });
